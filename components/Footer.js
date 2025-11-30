7efb675a598488a39de58f7ff89a4299e625d7d4
Nisarg Mistry <mistry.nisarg.24@gmail.com>
2025-11-30 15:32:05 +0530

    Buy Me A Chai

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-900 text-white flex justify-center px-4 items-center h-10'>
+      <p className='text-center'>Copyright &copy; Get me A Chai - All Rights Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
